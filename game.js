ced169159996116d9b10f7a014e13d83814f96ca
majidadindalawa <majidadindalawa@gmail.com>
2026-02-25 22:31:01 +0100

    Create game.js

---
+++ game.js
@@ -0,0 +1,13 @@
+ let score = 0;
+
+function increaseScore() {
+  score++;
+  console.log("Score: " + score);
+}
+
+function resetScore() {
+  score = 0;
+  console.log("Score reset!");
+}
+
+console.log("Game started!");
